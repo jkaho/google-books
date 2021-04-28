0e9825344845708ede2f3242b347c53613c3003f
jkaho <72959522+jkaho@users.noreply.github.com>
2021-04-28 21:20:26 +1000

    Add get request to Google Books API for specific book info

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,9 @@ const API = {
   searchBook: function(query) {
     return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}&maxResults=20&key=AIzaSyDKvDLYMy8N3_2BKyfyLke1o81felcyRuI`)
   },
+  getBookInfo: function(id) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes/${id}?key=AIzaSyDKvDLYMy8N3_2BKyfyLke1o81felcyRuI`)
+  }
 };
 
 export default API;
