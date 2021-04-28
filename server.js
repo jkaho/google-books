fa41ee1aaf4c3cb1d52e73ff6ed2a84201477e77
jkaho <72959522+jkaho@users.noreply.github.com>
2021-04-28 16:25:46 +1000

    Edit comments in server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+// Serve up static assets (on Heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// API Routes
 
 // Send every other request to the React app
 // Define any API routes before this runs
