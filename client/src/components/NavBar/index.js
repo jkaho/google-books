5db9f348a8ce94f7a53ce11b23e3454657263d88
jkaho <72959522+jkaho@users.noreply.github.com>
2021-04-28 17:20:26 +1000

    Create navbar component

---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "left",
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Google Books
+          </Typography>
+          <Button color="inherit">Search</Button>
+          <Button color="inherit">Saved</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
