799c1db13bf5da34dcc39545ca79a892f2099173
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-03 15:21:45 +1000

    Create and add footer component to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
-import "./App.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={Search} />
           <Route exact path="/saved" component={Saved} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-div">
+        <span>
+          <a href="https://www.github.com/jkaho/google-books" target="_blank" rel="noreferrer noopener">
+            Made by jkaho | 2021
+          </a>
+          </span>
+      </div>
+    </footer>
+  );
+};
