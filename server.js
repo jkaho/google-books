47dbdaa7f90228e5942ee64ca444e4ba4d495d34
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-04 17:42:18 +1000

    Deploy app to heroku

--- server.js
+++ server.js
@@ -18,7 +18,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to Mongo database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Send every other request to the React app
 // Define any API routes before this runs
